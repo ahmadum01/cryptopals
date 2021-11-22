117bb5bd71bbc5c05a9aa9dba0f3a9044849c7c3
Ahmad Umaev <ahmad.um.2001@mail.ru>
2021-11-22 21:25:08 +0300

    second commit

--- Set 1/Challenge_1.py
+++ Set 1/Challenge_1.py
@@ -39,6 +39,6 @@ def hex_to_base64(hex_str):
 		case _: None
 	return result
 
-
+s = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
 b64 = hex_to_base64(s)
 print('Third way:', b64)
