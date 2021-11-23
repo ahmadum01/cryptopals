b2edfc794d5ed1a621086491354db997ec750fdb
Ahmad Umaev <ahmad.um.2001@mail.ru>
2021-11-23 16:09:06 +0300

    Ревью кода и добавил тесты

---
+++ Set 1/Challenge1.py
@@ -0,0 +1,36 @@
+import base64
+hex_string = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
+
+
+# With lib
+def hex_to_base64_with_libs(hex_str):
+    return base64.b64encode(bytes.fromhex(hex_str)).decode()
+
+
+# My realization.
+def hex_to_base64(hex_str):
+    """Функция ковертирующая hex в base64"""
+    b64chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'  # Алфавит base64
+
+    if len(hex_str) % 2:  # Исключение выбрасывается, если был подан некорректный аргумент
+        raise ValueError('non-hexadecimal number found in hex_to_base64 arg')
+    for i in hex_str:
+        if i not in '0123456789ABCDEFabcdef':
+            raise ValueError('non-hexadecimal number found in hex_to_base64 arg')
+
+    # Преобразовываем hex строку в бинарную строку
+    bin_str = [bin(int(hex_str[i: i + 2], 16)) for i in range(0, len(hex_str), 2)]
+    bin_str = ''.join(['0' * (8 - len(i[2:])) + i[2:] for i in bin_str])
+
+    # Преобразовываем бинарную строку в строку base64
+    result = ''
+    for i in range(0, len(bin_str), 6):
+        match len(bin_str[i: i + 6]) % 3:
+            case 0: result += b64chars[int(bin_str[i: i + 6], 2)]
+            case 1: result += b64chars[int(bin_str[i:] + '00', 2)] + '='
+            case 2: result += b64chars[int(bin_str[i:] + '0000', 2)] + '=='
+    return result
+
+
+# print('With lib:', hex_to_base64_with_libs(hex_string))
+# print('My realization:', hex_to_base64(hex_string))
---
+++ Set 1/test_for_Challenge1.py
@@ -0,0 +1,37 @@
+import pytest
+from Challenge1 import *
+
+
+def test1():
+    hex_str = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
+    assert hex_to_base64(hex_str) == hex_to_base64_with_libs(hex_str)
+
+
+def test2():
+    hex_str = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f'
+    assert hex_to_base64(hex_str) == hex_to_base64_with_libs(hex_str)
+
+
+def test3():
+    hex_str = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f'
+    assert hex_to_base64(hex_str) == hex_to_base64_with_libs(hex_str)
+
+
+def test4():
+    hex_str = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f'.upper()
+    assert hex_to_base64(hex_str) == hex_to_base64_with_libs(hex_str)
+
+
+def test5():
+    hex_str = ''
+    assert hex_to_base64(hex_str) == hex_to_base64_with_libs(hex_str)
+
+
+def test6():
+    hex_str = 'aaaaaaaaaaaaaaaaaa'
+    assert hex_to_base64(hex_str) == hex_to_base64_with_libs(hex_str)
+
+
+def test7():
+    hex_str = '0000000000000000'
+    assert hex_to_base64(hex_str) == hex_to_base64_with_libs(hex_str)
